fff6611a8c05bd73877f1dee3c59a090f7cd7a53
Nicholas Sor <nick.sor@live.com>
2019-06-04 19:49:30 -0700

    Added more checks on when to flip and things to retain the list format when renaming

--- Program.cs
+++ Program.cs
@@ -17,15 +17,27 @@ namespace File_Renamer
 
             String Line;
             List<String> Names = new List<string>();
+
+            // Takes a list of names until the escape character is reached.
             System.Console.WriteLine("Enter list of names then ctrl+z and then enter.");
             while ((Line = Console.ReadLine()) != null) {
                 Names.Add(Line);
             }
-            System.Console.WriteLine(@"Are the names in ""Last, First"" (Y/N)");
-            // String Order = Console.ReadLine();
-            if(Console.ReadLine().ToUpper() == "Y")
+
+            // Gets the format of the names in the list provided.
+            System.Console.WriteLine(@"Are the names in the list ""Last, First""? (Y/N)");
+            String NameFormat = Console.ReadLine().ToUpper()[0].ToString();
+
+            // Gets the format of the file names.
+            System.Console.WriteLine("Do the files contain part of the first name or last name? (F/L)?");
+            String FileFormat = Console.ReadLine().ToUpper()[0].ToString();
+
+            // Flips the list of names to match the file format.
+            Boolean Flipped = false;
+            if((NameFormat == "Y" && FileFormat == "F") || (NameFormat == "N" && FileFormat == "L"))
             {
                 Names = Flipper(Names);
+                Flipped = true;
             }
 
             // Get the directory and the files to rename
@@ -42,15 +54,23 @@ namespace File_Renamer
 
                 foreach(String Name in Names)
                 {
+                    // Retains the list given by user.
+                    String TempName = Name;
+                    if(Flipped)
+                    {
+                        TempName = Flipper(Name);
+                    }
+
+                    // Renames the file
                     if(Name.ToLower().StartsWith(FileName.ToLower()))
                     {
                         int Count = 1;
-                        String NewFileName = CurrentFile.FullName.Replace(CurrentFile.Name, $"{Name}{Ex}");
+                        String NewFileName = CurrentFile.FullName.Replace(CurrentFile.Name, $"{TempName}{Ex}");
 
                         // Checks to see if a file with the same name exists.
                         while(File.Exists(NewFileName))
                         {
-                            String TempFileName = $"{Name} ({Count++}){Ex}";
+                            String TempFileName = $"{TempName} ({Count++}){Ex}";
                             NewFileName = CurrentFile.FullName.Replace(CurrentFile.Name, TempFileName);
                         }
                         File.Move(CurrentFile.FullName, NewFileName);
@@ -60,6 +80,7 @@ namespace File_Renamer
             }
         }
 
+        // Switches the format of the names.
         public static List<String> Flipper(List<String> Names)
         {
             List<String> ReversedNames = new List<String>();
@@ -88,6 +109,31 @@ namespace File_Renamer
                 ReversedNames.Add(Reversed);
             }
             return ReversedNames;
+        }
+        
+        // Switches the format of a single name
+        public static String Flipper(String Name)
+        {
+            String NameOne = "";
+            String NameTwo = "";
+            Boolean Switch = false;
+            for(int i = 0; i < Name.Length; i++)
+            {
+                if(Name[i].ToString() == ",")
+                {
+                    Switch = true;
+                    i++;
+                }
+                if(!Switch)
+                {
+                    NameOne += Name[i];
+                }
+                else
+                {
+                    NameTwo += Name[i];
+                }
+            }
+            return $"{NameTwo}, {NameOne}".Trim();
         }
     }
 }
